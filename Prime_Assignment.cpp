5a5afbf4dd943426e4ee49144502e5c4e397b9c4
ghazalrafiei <gh.rafiei99@gmail.com>
2023-01-21 15:28:31 +0330

    Add assignment & workflow test

--- Prime_Assignment.cpp
+++ Prime_Assignment.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 
 bool is_prime(int n){
-    // imeplement HERE.
-    return true;
+    if (n==2)
+        return true;
+    
+    for(int i = 2; i<n; i++){
+        if(n%i==0){
+            return true;
+        }
+    }
+    return false;
 }
 
 
 int main(){
+    std::cout<<is_prime(12)<<'\n';
+    std::cout<<is_prime(2)<<'\n';
+    std::cout<<is_prime(3)<<'\n';
+    std::cout<<is_prime(15)<<'\n';
+    std::cout<<is_prime(127);
     return 0;
 }
