4ea85a142e3cf24c1235f316ba91945c2fbbc691
ghazalrafiei <gh.rafiei99@gmail.com>
2023-01-21 16:03:12 +0330

    xxx

--- build/TwoTest[1]_tests.cmake
+++ build/TwoTest[1]_tests.cmake
@@ -1,1 +1,3 @@
-set(  TwoTest_TESTS)
+add_test([=[TwoTest.PrintTwo]=]  /home/giraffael/Documents/E658/gittest/build/TwoTest [==[--gtest_filter=TwoTest.PrintTwo]==] --gtest_also_run_disabled_tests)
+set_tests_properties([=[TwoTest.PrintTwo]=]  PROPERTIES WORKING_DIRECTORY /home/giraffael/Documents/E658/gittest/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
+set(  TwoTest_TESTS TwoTest.PrintTwo)
