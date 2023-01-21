7f199c4e43019f40f1cdb89c6189187188b56a4e
ghazalrafiei <gh.rafiei99@gmail.com>
2023-01-21 16:03:20 +0330

    xxx

--- test/two_test.cpp
+++ test/two_test.cpp
@@ -1,14 +1,19 @@
 #include <gtest/gtest.h>
 #include "../two.cpp"
+// #include "../Prime_Assignment.cpp"
 
 // Demonstrate some basic assertions.
 // Optional names without _
 // TestSuiteName, TestName
 TEST(TwoTest, PrintTwo) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  ASSERT_EQ(4, 4);
-  // Expect equality.
-  // EXPECT_EQ(1+1, 2);
   EXPECT_EQ(ReturnTwo(4), 2);
+}
+
+// TEST(Prime, prime){
+//  EXPECT_FALSE(is_prime(12));
+//  }
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
--- two.cpp
+++ two.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include "gtest/gtest.h"
+// #include "Prime_Assignment.cpp"
 
 int ReturnTwo(int t){
     int x = 2;
     return x;
 }
 
-int main(){
-    std::cout<<"Hello World.";
+bool is_prime(int n){
+    if (n==2)
+        return true;
+    
+    for(int i = 2; i<n; i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
 }
+
+// int main(){
+//     std::cout<<"Hello World.";
+//     return 0;
+//     // std::cout<<is_prime(12)<<'\n';
+//     // std::cout<<is_prime(2)<<'\n';
+//     // std::cout<<is_prime(3)<<'\n';
+//     // std::cout<<is_prime(15)<<'\n';
+//     // std::cout<<is_prime(127);
+// }
