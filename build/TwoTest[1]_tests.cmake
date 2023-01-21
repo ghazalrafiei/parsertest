79b082900b9420c71e217ffa0da9c9fa2381e353
Ghazal Rafiei <gh.rafiei99@gmail.com>
2023-01-21 17:02:45 +0330

    Delete build directory

--- build/TwoTest[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[TwoTest.PrintTwo]=]  /home/giraffael/Documents/E658/gittest/build/TwoTest [==[--gtest_filter=TwoTest.PrintTwo]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[TwoTest.PrintTwo]=]  PROPERTIES WORKING_DIRECTORY /home/giraffael/Documents/E658/gittest/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  TwoTest_TESTS TwoTest.PrintTwo)
